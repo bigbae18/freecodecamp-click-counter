dcaec7668e5f7c5625c63bde8b4cee49cb9b9c9d
Adrián Pelayo <niniohhns@gmail.com>
2022-06-09 13:30:01 +0200

    Little react fix on clickcounter

--- src/components/ClickCounter.js
+++ src/components/ClickCounter.js
@@ -18,7 +18,7 @@ const ClickCounter = () => {
     return (
         <>
             <h1>{count}</h1>
-            <div class="button-container">
+            <div className="button-container">
                 <Button handler={handleAddCount} isReset={false}>Click to Add 1!</Button>
                 <Button handler={handleReset} isReset={true}>Reset to 0!</Button>
                 <Button handler={handleSubstractCount} isReset={false}>Click to Substract 1!</Button>
